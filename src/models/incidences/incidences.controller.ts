0d438f590352af017221679820421e81ce637b41
Rafael Estrada <ntaticat@gmail.com>
2022-12-01 18:57:01 -0600

    changes

--- src/models/incidences/incidences.controller.ts
+++ src/models/incidences/incidences.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Patch, Post } from '@nestjs/common';
 import { IncidencesService } from './incidences.service';
 
 @Controller('incidences')
@@ -26,6 +26,12 @@ export class IncidencesController {
   @Patch(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
   updateIncidence(@Param("id") id: string, @Body() data: any) {
+
+    if(id === "1") {
+      throw new NotFoundException("Ese Id no fue encontrado");
+      
+    }
+
     return null;
   }
 
